e58fd98f299eebd3e11d4492a04b749f86d48e88
Samanta Suto <samantasuto@gamil.com>
2023-06-07 17:20:05 -0300

    exemplos da aula 1

---
+++ tiponumber.js
@@ -0,0 +1,24 @@
+// tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNUmero = 2;
+
+const operaçãoMatematica = primeiroNumero + segundoNUmero;
+
+// console.log(operaçãoMatematica)
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const numeroPontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+//console.log(novaOperacao)
+
+// NaN . Not A Number ( não é um número)
+
+const alura = 'Alura'
+console.log(alura + primeiroNumero)
---
+++ tipostring.js
@@ -0,0 +1,11 @@
+const testo1 = "Olá Mundo";
+const texto2 = 'Olá Mundo';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao = "Meu nome é";
+const meuNome = " Samanta"
+
+// concatenação (+)
+
+console.log (citacao + meuNome)
