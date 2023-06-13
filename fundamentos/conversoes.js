6fc1e19e5af932908e46ca126f7a543a8fb9c181
Samanta Suto <samantasuto@gamil.com>
2023-06-13 15:35:18 -0300

    var e conversoes

---
+++ fundamentos/conversoes.js
@@ -0,0 +1,18 @@
+// topo de dado
+// booleanos
+
+// conversão implicíta
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero + Number(numeroString))
+
+// = atribuir valor a uma variável
+// == comparação pelo valor (somente o que está dentro da variável)
+// === compara não só o valor mas tbm o tipo ( se é string ou number), só atrinuira true se os dois
+// forem iguais, 2 tipo numeber, ou os dois tipo string
+
+// conversão explicita
+
+// number () = transforma string em numero (somente strings que contenha somente números)
+// string () = tansforma numero em string
---
+++ fundamentos/truthyfalse.js
@@ -0,0 +1,39 @@
+//boolean
+const usurioLogado = true
+const contaPaga = false
+
+// truthy ou falsy
+
+ // 0 => false
+ // 1 => true
+
+ //console.log(0 == false)
+ //console.log("" == false)
+ //console.log(1 == true)
+
+  // undefined
+
+ // null => vazio ou nada
+
+ let minhaVar;
+ let varNull = null;
+ 
+ //console.log(minhaVar)
+ //console.log(varNull)
+
+ let numero = 3;
+ let texto = "Alura";
+
+ //console.log(typeof numero)
+ //console.log(typeof texto)
+
+ 
+ console.log(typeof minhaVar)
+ console.log(typeof varNull) 
+ 
+ //null foi criado como objeto, e não será redefido como null, pois quebraria vários códigos existentes.
+ 
+ 
+
+
+ 
